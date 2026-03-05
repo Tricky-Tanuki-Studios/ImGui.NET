dd9ea42996b5f0816765a49104e6221d4a7d2149
asoji <git@asoji.gay>
2026-03-05 00:11:24 -0800

    manual `ImDrawTextFlags` flag, autogen later pls

---
+++ TrickyTanukiStudios.ImGui.NET/ImDrawTextFlags.Manual.cs
@@ -0,0 +1,10 @@
+﻿namespace ImGuiNET {
+    // if this can later be turned into something just generated, that'd be preferred
+    [System.Flags]
+    public enum ImDrawTextFlags {
+        None = 0,
+        CpuFineClip = 1 << 0,
+        WrapKeepBlanks = 1 << 1,
+        StopOnNewLine = 1 << 2,
+    }
+}
